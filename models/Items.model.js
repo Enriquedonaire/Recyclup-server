12fbb99bcb2b95e41d0ff629fda62e4cb2da692f
gigi-loranca <85176906+gigi-loranca@users.noreply.github.com>
2021-07-23 16:43:21 +0200

    itemSchema re-added

---
+++ models/Items.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+
+const ItemSchema = new Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: {type : String},
+  description: {type: String},
+  available: {default: true},
+  image: {type: String}
+});
+
+const Item = model("Item", ItemSchema);
+
+module.exports = Item;
