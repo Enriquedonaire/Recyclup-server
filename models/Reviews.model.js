a1ef4df44c247cae6c050d95905009724daf5e9f
gigi-loranca <85176906+gigi-loranca@users.noreply.github.com>
2021-07-29 11:24:50 +0200

    client update

--- models/Reviews.model.js
+++ models/Reviews.model.js
@@ -4,18 +4,13 @@ const { Schema, model } = require("mongoose");
 const ReviewsSchema = new Schema({
     name: {
     type: String,
-        
     },
     description: {
         type: String,
     },
-
     ranking: {
         type: Number,
-
     }
-
-    
 });
 
 const Reviews = model("Item", ReviewsSchema);
